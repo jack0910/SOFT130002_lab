7355b12a4e8ce57290e1f6d8d01b1899fb81579d
jack0910 <19302010040@fudan.edu.cn>
2020-06-14 23:47:55 +0800

    add new lab

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBUSER', 'gsh');
+define('DBPASS', 'Gsh091099');
+define('DBCONNSTRING','mysql:host=localhost;dbname=login;charset=utf8mb4;');
+?>
